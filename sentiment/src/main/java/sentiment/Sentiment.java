74c5b935275c863cbb5758edc70d08707c391d42
Martin Kalany <m.kalany@gmx.at>
2013-11-09 19:26:14 +0100

    add interface for sentiment analysis

---
+++ sentiment/src/main/java/sentiment/Sentiment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sentiment;
+
+/**
+ *
+ * @author martin
+ */
+public class Sentiment implements ISentiment {
+
+    public double analyse(String key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
