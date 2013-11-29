c5d3dffb27b344553fa48903a8d245946bdee27f
soodeh.farokhi@tuwien.ac.at <soodeh.farokhi@tuwien.ac.at>
2013-11-29 14:21:11 +0100

    @DestructCloudObject. annotation added to the task class, main method.

--- sentiment/src/main/java/main/Task.java
+++ sentiment/src/main/java/main/Task.java
@@ -4,11 +4,12 @@ import java.util.Date;
 
 import at.ac.tuwien.infosys.cloudscale.annotations.ByValueParameter;
 import at.ac.tuwien.infosys.cloudscale.annotations.CloudObject;
+import at.ac.tuwien.infosys.cloudscale.annotations.DestructCloudObject;
 import twitter.TwitterSentimentAnalyzer;
 
 @CloudObject
 public class Task {
-	
+	@DestructCloudObject
 	public @ByValueParameter double run(@ByValueParameter String key,@ByValueParameter Date since,@ByValueParameter Date until)
 	{
 		TwitterSentimentAnalyzer analyzier = new TwitterSentimentAnalyzer();
