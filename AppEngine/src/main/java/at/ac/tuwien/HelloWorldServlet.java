7b2d2d7ee71f048c302b1c699c235f95f7a9cf6d
raduke <gonz.o@a1.net>
2013-12-25 03:15:50 +0100

    Initial commit, AppEngine

---
+++ AppEngine/src/main/java/at/ac/tuwien/HelloWorldServlet.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
