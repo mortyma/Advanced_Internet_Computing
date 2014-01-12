fb455392a4966f8a375dfbf801ef2aa57313d25c
Martin Kalany <m.kalany@gmx.at>
2014-01-12 19:05:13 +0100

    use task queue to process sentiment requests

--- AppEngine/src/main/java/appengine/SentimentRequestServlet.java
+++ AppEngine/src/main/java/appengine/SentimentRequestServlet.java
@@ -5,46 +5,38 @@
  */
 package appengine;
 
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import static com.google.appengine.api.taskqueue.TaskOptions.Builder.*;
 import java.io.IOException;
 import java.util.logging.Logger;
 import javax.servlet.http.*;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import java.io.InputStream;
 import java.util.logging.Level;
 import javax.servlet.ServletContext;
-import sentiment.TwitterSentimentAnalyzer;
 
-public class SentimentRequest extends HttpServlet implements IResourceLocator {
+public class SentimentRequestServlet extends HttpServlet implements IResourceLocator {
 
-    private static final Logger log = Logger.getLogger(SentimentRequest.class.getName());
+    private static final Logger log = Logger.getLogger(SentimentRequestServlet.class.getName());
 
     ServletContext context;
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
-
         String key = req.getParameter("key");
         String since = req.getParameter("since");
         String until = req.getParameter("until");
 
-        //TODO: error handling
+        //TODO: error handling, validate parameters
         log.log(Level.INFO, "Request initiated: {0}, {1}, {2}", new Object[]{key, since, until});
 
         context = req.getSession(true).getServletContext();
 
-        TwitterSentimentAnalyzer analyzier = new TwitterSentimentAnalyzer(this);
-        double value = 0;
-        //TODO: using the method with java.util.Date doesn't work
-        value = analyzier.sentimentFor(key, since, until);
-
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Sentiment for " + key + ": " + value);
-        //resp.sendRedirect("/SentimentAnalysis.jsp");
+        Queue queue = QueueFactory.getQueue("sentiment-queue");
+        queue.add(withUrl("/SentimentProcessorTask").param("key", key).param("since", since).param("until", until));
+       
+        resp.sendRedirect("/SentimentAnalysis.jsp");
     }
 
       
