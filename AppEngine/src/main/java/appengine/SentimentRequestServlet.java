c449a1c58ed55f85623c9e5c86153dff6b6ed595
Martin Kalany <m.kalany@gmx.at>
2014-01-12 19:35:48 +0100

    SentimentRequestServlet no longer needs to implement IResourceLocator

--- AppEngine/src/main/java/appengine/SentimentRequestServlet.java
+++ AppEngine/src/main/java/appengine/SentimentRequestServlet.java
@@ -11,16 +11,12 @@ import static com.google.appengine.api.taskqueue.TaskOptions.Builder.*;
 import java.io.IOException;
 import java.util.logging.Logger;
 import javax.servlet.http.*;
-import java.io.InputStream;
 import java.util.logging.Level;
-import javax.servlet.ServletContext;
 
-public class SentimentRequestServlet extends HttpServlet implements IResourceLocator {
+public class SentimentRequestServlet extends HttpServlet {
 
     private static final Logger log = Logger.getLogger(SentimentRequestServlet.class.getName());
-
-    ServletContext context;
-
+    
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
@@ -31,25 +27,10 @@ public class SentimentRequestServlet extends HttpServlet implements IResourceLoc
         //TODO: error handling, validate parameters
         log.log(Level.INFO, "Request initiated: {0}, {1}, {2}", new Object[]{key, since, until});
 
-        context = req.getSession(true).getServletContext();
-
+   
         Queue queue = QueueFactory.getQueue("sentiment-queue");
         queue.add(withUrl("/SentimentProcessorTask").param("key", key).param("since", since).param("until", until));
        
         resp.sendRedirect("/SentimentAnalysis.jsp");
-    }
-
-      
-    @Override
-    public InputStream getResource(String file) {
-
-        if (context == null) {
-            throw new NullPointerException();
-        }
-        InputStream in = context.getResourceAsStream(file);
-        if (in == null) {
-            throw new IllegalArgumentException("can't open file: " + file);
-        }
-        return in;
     }
 }
