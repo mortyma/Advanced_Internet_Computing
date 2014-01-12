471f89c0385a98699a0c77017b15d1428c33ccc7
Martin Kalany <m.kalany@gmx.at>
2014-01-12 19:52:26 +0100

    store requests in datastore

--- AppEngine/src/main/java/appengine/SentimentRequestServlet.java
+++ AppEngine/src/main/java/appengine/SentimentRequestServlet.java
@@ -5,6 +5,11 @@
  */
 package appengine;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import static com.google.appengine.api.taskqueue.TaskOptions.Builder.*;
@@ -27,10 +32,28 @@ public class SentimentRequestServlet extends HttpServlet {
         //TODO: error handling, validate parameters
         log.log(Level.INFO, "Request initiated: {0}, {1}, {2}", new Object[]{key, since, until});
 
-   
+        //TODO: what is kind and name?
+        //Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
+        /*
+            Create an entry in the task store
+        */
+        Key db_key = KeyFactory.createKey("Sentiment", "default");
+        Entity request = new Entity("request", db_key);
+        request.setProperty("key", key);
+        request.setProperty("since", since);
+        request.setProperty("until", until);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key request_key = datastore.put(request);
+        
+        /*
+            Create a new task to do the compulationally intensitive work 
+            of calculating the sentiment
+        */
         Queue queue = QueueFactory.getQueue("sentiment-queue");
+        //TODO: pass along request_key
         queue.add(withUrl("/SentimentProcessorTask").param("key", key).param("since", since).param("until", until));
        
+        //TODO: give some sort of confirmation to the user
         resp.sendRedirect("/SentimentAnalysis.jsp");
     }
 }
