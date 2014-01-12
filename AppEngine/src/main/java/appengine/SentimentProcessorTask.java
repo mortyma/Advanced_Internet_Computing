6d49406ca5d9b9290e6cf6eab17cea89f0f3f7b4
Martin Kalany <m.kalany@gmx.at>
2014-01-12 20:21:41 +0100

    task fetches request from datastore

--- AppEngine/src/main/java/appengine/SentimentProcessorTask.java
+++ AppEngine/src/main/java/appengine/SentimentProcessorTask.java
@@ -3,11 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package appengine;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.apphosting.datastore.EntityV4;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
@@ -29,24 +38,41 @@ public class SentimentProcessorTask extends HttpServlet implements IResourceLoca
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-       
-        String key = req.getParameter("key");
-        String since = req.getParameter("since");
-        String until = req.getParameter("until");
 
+        /*
+            we need the context to access files on the web server
+         */
         context = req.getSession(true).getServletContext();
 
-       
+        //get the request parameters from the datastore
+        Key request_key = KeyFactory.stringToKey(req.getParameter("request_key"));
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        Entity request;
+        try {
+            request = datastore.get(request_key);
+        } catch (EntityNotFoundException ex) {
+            log.log(Level.SEVERE, null, ex);
+            return;
+        }
+
+        String key = (String) request.getProperty("key");
+        String since = (String) request.getProperty("since");
+        String until = (String) request.getProperty("until");
+
+        /*
+            Run sentiment analysis
+        */
         TwitterSentimentAnalyzer analyzier = new TwitterSentimentAnalyzer(this);
-        double value = 0;
         //TODO: using the method with java.util.Date doesn't work
-        value = analyzier.sentimentFor(key, since, until);
-       
+        double value = analyzier.sentimentFor(key, since, until);
+
+        log.log(Level.INFO, "Sentiment for {0}: {1}", new Object[]{key, value});               
+        
         //TODO: write to datastore       
-         log.log(Level.INFO, "Sentiment for {0}: {1}", new Object[]{key, value});
+        
     }
 
-      
     @Override
     public InputStream getResource(String file) {
 
